800d27788d9d6c4a6ffb1ede90c2c469bf67de6e
Yune Su Nadi <yunesunadi.dev@gmail.com>
2026-08-13 21:50:33 +0700

    changed to zoneless

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -11,7 +11,7 @@ import { unauthorizedInterceptor } from './interceptors/unauthorized.interceptor
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZonelessChangeDetection(),
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(withXhr(), withInterceptors([
