772965f63e2d279ff5c80a467558d8696659f9cd
Yune Su Nadi <yunesunadi.dev@gmail.com>
2026-08-14 19:33:54 +0700

    updated guard

--- src/app/guards/complete-auth.guard.ts
+++ src/app/guards/complete-auth.guard.ts
@@ -1,10 +1,10 @@
 import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { CanMatchFn, Router } from '@angular/router';
 import { map } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import { DashboardCacheService } from '../caches/dashboard-cache.service';
 
-export const completeAuthGuard: CanActivateFn = (route, state) => {
+export const completeAuthGuard: CanMatchFn = (route, segments) => {
   const authService = inject(AuthService);
   const cacheService = inject(DashboardCacheService);
   const router = inject(Router);
@@ -19,7 +19,7 @@ export const completeAuthGuard: CanActivateFn = (route, state) => {
         return router.parseUrl('/role');
       }
 
-      const expected_role = state.url.startsWith('/organizer') ? 'organizer' : 'attendee';
+      const expected_role = route.path?.startsWith('organizer') ? 'organizer' : 'attendee';
 
       if (res.role !== expected_role) {
         return router.parseUrl(`/${res.role}/dashboard/home`);
