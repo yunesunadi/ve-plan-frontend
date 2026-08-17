9f7dc908a552b1bb6400961ba2d54d7d8c978022
Yune Su Nadi <yunesunadi.dev@gmail.com>
2026-08-17 16:28:08 +0700

    updated config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:5000/api/v1",
-  socketUrl: "http://localhost:5000",
-  profileUrl: "http://localhost:5000/api/v1/static/profiles",
-  coverUrl: "http://localhost:5000/api/v1/static/covers",
+  apiUrl: "https://ve-plan-backend.onrender.com/api/v1",
+  socketUrl: "https://ve-plan-backend.onrender.com",
+  profileUrl: "https://ve-plan-backend.onrender.com/api/v1/static/profiles",
+  coverUrl: "https://ve-plan-backend.onrender.com/api/v1/static/covers",
   appId: "vpaas-magic-cookie-0e894e017ee743f6a9324a95d07ad582",
   meeting_domain: "8x8.vc",
-  google_oauth_url: "http://localhost:5000/api/v1/auth/google",
-  facebook_oauth_url: "http://localhost:5000/api/v1/auth/facebook",
+  google_oauth_url: "https://ve-plan-backend.onrender.com/api/v1/auth/google",
+  facebook_oauth_url: "https://ve-plan-backend.onrender.com/api/v1/auth/facebook",
 };
